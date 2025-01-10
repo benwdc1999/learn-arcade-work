f5cba39f29db3d1a1b0f0214b7cd8cf0680201fb
benjamin <benbhbe@yahoo.com>
2025-01-09 19:50:31 -0500

    Work on lab 1

--- Lab 01 - First Program/lab_01.py
+++ Lab 01 - First Program/lab_01.py
@@ -1,1 +1,5 @@
-Hi
+print("\"Fourscore and seven yeas ago..\"\nAbe Lincoln")
+print("That was the Gettysburg address")
+print("I have passed through Gettysburg on my way to other places.")
+print("When I was there I saw civil war reenactors.")
+
