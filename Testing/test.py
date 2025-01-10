7cfe60ec4bb196427b1008119914746bcabe13b3
benjamin <benbhbe@yahoo.com>
2025-01-09 19:23:04 -0500

    Work on printing

---
+++ Testing/test.py
@@ -0,0 +1,16 @@
+print ("It was a dark and stormy night.")
+print("Suddenly a shot rang out!")
+
+print("This\nis\nmy\nsample.")
+
+print("""You
+can
+print
+on
+multiple
+lines
+using
+triple
+quotes""")
+
+
